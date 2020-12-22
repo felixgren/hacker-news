25692a378d31ba217531d2ee35b1449a9a452b8d
felixgren <felixg.contact@gmail.com>
2020-12-22 17:28:03 +0100

    Styled mobile posts view

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,28 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 
-<body>
+<body class="bg-gray-200">
+    <nav class="bg-hacker-orange flex flex-wrap sm:flex-nowrap sm:justify-between p-2 mb-6">
+        <ul class="flex items-center">
+            <li>
+                <a href="" class="text-sm pr-3 sm:text-base sm:p-3">Home</a>
+            </li>
+            <li>
+                <a href="" class="text-sm pr-3 sm:text-base sm:p-3">Comments</a>
+            </li>
+        </ul>
+
+        <h1 class="font-mono font-bold order-first sm:order-none w-full sm:w-auto sm:text-xl">Hacker News</h1>
+
+        <ul class="flex items-center">
+            <li>
+                <a href="" class="text-sm pr-3 sm:text-base sm:p-3">Login</a> {{-- Login / Username --}}
+            </li>
+            <li>
+                <a href="" class="text-sm pr-3 sm:text-base sm:p-3">Register</a> {{-- Register / Logout --}}
+            </li>
+        </ul>
+    </nav>
     @yield('content')
 </body>
 
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,7 +2,9 @@
 @extends('layouts.app') {{-- . same as /, just different notation --}}
 
 @section('content')
-<div class="bg-hacker-orange">
-    Test
+<div class="flex justify-center">
+    <div class="w-10/12 bg-white p-3 {{-- w-8/12 bg-white p-6 rounded-lg --}}">
+        Posts yes yes
+    </div>
 </div>
 @endsection
