02761d24a7b02792dbe97e8a252e3c0ff9e32430
felixgren <felixg.contact@gmail.com>
2020-12-26 04:17:51 +0100

    Updated form styling, SR support, dark mode

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,38 +2,38 @@
 
 @section('content')
 <div class="flex justify-center">
-    <div class="w-10/12 bg-white p-3 sm:p-6 sm:rounded-lg md:max-w-screen-md xl:max-w-screen-lg dark:text-white dark:bg-transparent dark:border-solid border border-white border-opacity-40">
-        <h2 class="">Registration</h2>
+    <div class="w-10/12 bg-white p-3 sm:p-6 sm:rounded-lg md:max-w-screen-md xl:max-w-screen-lg dark:text-white dark:bg-transparent dark:border-solid border border-white border-opacity-20">
+        <h2 class="">Register</h2>
 
         <form action="">
-            <div class="my-2">
-                <label for="name"></label>
+            <div class="my-4">
+                <label for="name" class="sr-only">Name</label>
                 <input type="text" name="name" id="name" placeholder="Name"
-                class="bg-gray-200 w-full p-1" value="">
+                class="bg-gray-100 border-solid border border-black border-opacity-40 w-full p-1 rounded-sm dark:bg-transparent dark:border-white" value="">
             </div>
 
-            <div class="my-2">
-                <label for="username"></label>
+            <div class="my-4">
+                <label for="username" class="sr-only">Username</label>
                 <input type="text" name="username" id="username" placeholder="Username"
-                class="bg-gray-200 w-full p-1" value="">
+                class="bg-gray-100 border-solid border border-black border-opacity-40 w-full p-1 rounded-sm dark:bg-transparent dark:border-white" value="">
             </div>
 
-            <div class="my-2">
-                <label for="email"></label>
+            <div class="my-4">
+                <label for="email" class="sr-only">Email</label>
                 <input type="text" name="email" id="email" placeholder="Email"
-                class="bg-gray-200 w-full p-1" value="">
+                class="bg-gray-100 border-solid border border-black border-opacity-40 w-full p-1 rounded-sm dark:bg-transparent dark:border-white" value="">
             </div>
 
-            <div class="my-2">
-                <label for="password"></label>
+            <div class="my-4">
+                <label for="password" class="sr-only">Password</label>
                 <input type="password" name="password" id="password" placeholder="Choose password"
-                class="bg-gray-200 w-full p-1" value="">
+                class="bg-gray-100 border-solid border border-black border-opacity-40 w-full p-1 rounded-sm dark:bg-transparent dark:border-white" value="">
             </div>
 
-            <div class="my-2">
-                <label for="password_confirmation"></label>
-                <input type="text" name="password_confirmation" id="password_confirmation" placeholder="Confirm password"
-                class="bg-gray-200 w-full p-1" value="">
+            <div class="my-4">
+                <label for="password_confirmation" class="sr-only">Confirm Password</label>
+                <input type="password" name="password_confirmation" id="password_confirmation" placeholder="Confirm password"
+                class="bg-gray-100 border-solid border border-black border-opacity-40 w-full p-1 rounded-sm dark:bg-transparent dark:border-white" value="">
             </div>
         </form>
     </div>
