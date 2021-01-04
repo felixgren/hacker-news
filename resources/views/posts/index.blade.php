9cdc62469b1abadcbd56c82257b7dc11c59f25ed
felixgren <felixg.contact@gmail.com>
2021-01-04 20:00:28 +0100

    Created component of posts view
    Provides ability to reuse as component across several views

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -35,46 +35,7 @@
         @if ($count = $posts->count()) 
             <p> We have {{$count}} posts!</p>
                 @foreach ($posts as $post)
-                <div class="bg-gray-200 my-2">
-                    <div class="flex justify-between">
-                        <div class="">
-                            <a href="{{ route('users.posts', $post->user) }}" class="font-bold">{{ $post->user->name }}</a>
-                            <span>says...</span>
-                        </div>
-                        <span class="text-xs">{{ $post->created_at->diffForHumans()}}</span> {{-- Carbon PHP API is used for DateTime --}}
-                    </div>
-
-                    <p>{{ $post->body}}</p>
-
-                    <div class="flex flex-wrap items-center">
-                    @auth
-                        @can('delete', $post)
-                        <div class="w-full">
-                            <form action="{{ route('posts.destroy', $post) }}" method="post">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" class="text-blue-500 text-sm">Delete</button>
-                            </form>
-                        </div>
-                        @endcan
-
-                        @if (!$post->LikedBy(auth()->user()))
-                        <form action="{{ route('posts.likes', $post) }}" method="post" class="mr-1">
-                            @csrf
-                            <button type="submit" class="text-blue-500 text-sm">Like</button>
-                        </form>
-                        @else
-                        <form action="{{ route('posts.likes', $post) }}" method="post" class="mr-1">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="text-blue-500 text-sm">Unlike</button>
-                        </form>
-                        @endif
-                    @endauth
-
-                        <span class="text-sm">{{ $post->likes->count() }}</span>
-                    </div>
-                </div>
+                    <x-post :post="$post" />
                 @endforeach
 
                 {{ $posts->links() }} {{-- Included tailwind view --}}
