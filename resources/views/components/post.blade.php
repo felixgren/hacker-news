83acc0b85df30920d20431ab18b8f536f04f0e30
felixgren <felixg.contact@gmail.com>
2021-01-06 17:55:59 +0100

    Moved post edit to single post view

--- resources/views/components/post.blade.php
+++ resources/views/components/post.blade.php
@@ -14,31 +14,6 @@
     <div class="flex flex-wrap items-center">
     @auth
         @can('update', $post)
-        <div class="w-full flex ">
-            <form action="{{ route('posts.edit', $post) }}" method="post" class="mt-2">
-                @csrf
-                @method('PATCH')
-                <div>
-                    <label for="edit body" class="sr-only">Edit comment</label>
-                    <textarea name="body" id="body" cols="30" rows="1" class="bg-transparent
-                    border-2 border-gray-300 w-full p-2 rounded-sm text-opacity-50 @error('body') border-red-500 @enderror"
-                    placeholder="Edit comment">{{ $post->body }}</textarea>
-
-                    @error('body') 
-                        <div class="text-red-500 mt-1 text-sm">
-                            {{ $message }}
-                        </div>
-                    @enderror
-                </div>
-
-                <div>
-                    <button type="submit" class="bg-hacker-orange text-white py-1 mt-1 rounded-sm w-1/4 dark:bg-dark-gh-btn">Edit</button>
-                </div>
-            </form>
-        </div>
-        @endcan
-
-        @can('update', $post)
         <div class="w-full">
             <form action="{{ route('posts.destroy', $post) }}" method="post">
                 @csrf
