607d0e9cf0c363ce11126d27bd23ece68b03cc84
felixgren <felixg.contact@gmail.com>
2021-01-08 22:37:57 +0100

    Updated user settings form

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,55 +6,53 @@
         class="w-10/12 bg-white p-3 sm:p-6 sm:rounded-lg md:max-w-screen-md xl:max-w-screen-lg dark:text-white dark:bg-transparent dark:border-solid border border-white border-opacity-40">
         <h1 class="text-lg"><b>Welcome to your dashboard {{ $user->name }}</b></h1>
 
-        <h2>Settings</h2>
+        <h2>Update your settings</h2>
 
-        <div class="my-4">
-            <label for="name" class="sr-only">Name</label>
-            <input type="text" name="name" id="name" value="{{ old('name') ? old('name') : $user->name }}"
-            class="bg-gray-100 border-solid border border-black border-opacity-40 w-full p-1 rounded-sm dark:bg-transparent dark:border-white @error('name') border-red-500 border-opacity-100  @enderror" value="{{ old('name') }}">
+        <form action="" method="post">
+        @csrf
+            <div class="my-4">
+                <label for="username">Username</label>
+                <input type="text" name="username" id="username" value="{{ old('username') ? old('username') : $user->username }}"
+                class="bg-gray-100 border-solid border border-black border-opacity-40 w-full p-1 rounded-sm dark:bg-transparent dark:border-white @error('username') border-red-500 border-opacity-100  @enderror">
 
-            @error('name') 
-            <div class="text-red-500 mt-1 text-sm">
-                {{ $message }}
+                @error('username') 
+                <div class="text-red-500 mt-1 text-sm">
+                    {{ $message }}
+                </div>
+                @enderror
             </div>
-            @enderror
-        </div>
 
-        <div class="my-4">
-            <label for="username" class="sr-only">Username</label>
-            <input type="text" name="username" id="username" placeholder="Username"
-            class="bg-gray-100 border-solid border border-black border-opacity-40 w-full p-1 rounded-sm dark:bg-transparent dark:border-white @error('name') border-red-500 border-opacity-100  @enderror" value="{{ old('username') }}">
+            <div class="my-4">
+                <label for="email">Email</label>
+                <input type="text" name="email" id="email" value="{{ old('email') ? old('email') : $user->email }}"
+                class="bg-gray-100 border-solid border border-black border-opacity-40 w-full p-1 rounded-sm dark:bg-transparent dark:border-white @error('email') border-red-500 border-opacity-100  @enderror">
 
-            @error('username') 
-            <div class="text-red-500 mt-1 text-sm">
-                {{ $message }}
+                @error('email') 
+                <div class="text-red-500 mt-1 text-sm">
+                    {{ $message }}
+                </div>
+                @enderror
             </div>
-            @enderror
-        </div>
 
-        <div class="my-4">
-            <label for="email" class="sr-only">Email</label>
-            <input type="text" name="email" id="email" placeholder="Email"
-            class="bg-gray-100 border-solid border border-black border-opacity-40 w-full p-1 rounded-sm dark:bg-transparent dark:border-white @error('name') border-red-500 border-opacity-100  @enderror" value="{{ old('email') }}">
-
-            @error('email') 
-            <div class="text-red-500 mt-1 text-sm">
-                {{ $message }}
+            {{-- <div class="my-4">
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password" placeholder="Choose password"
+                class="bg-gray-100 border-solid border border-black border-opacity-40 w-full p-1 rounded-sm dark:bg-transparent dark:border-white @error('name') border-red-500 border-opacity-100  @enderror">
+                @error('password') 
+                <div class="text-red-500 mt-1 text-sm">
+                    {{ $message }}
+                </div>
+                @enderror
             </div>
-            @enderror
-        </div>
-
-        <div class="my-4">
-            <label for="password" class="sr-only">Password</label>
-            <input type="password" name="password" id="password" placeholder="Choose password"
-            class="bg-gray-100 border-solid border border-black border-opacity-40 w-full p-1 rounded-sm dark:bg-transparent dark:border-white @error('name') border-red-500 border-opacity-100  @enderror" value="">
-
-            @error('password') 
-            <div class="text-red-500 mt-1 text-sm">
-                {{ $message }}
+            <div class="my-4">
+                <label for="password_confirmation" class="sr-only">Confirm Password</label>
+                <input type="password" name="password_confirmation" id="password_confirmation" placeholder="Confirm password"
+                class="bg-gray-100 border-solid border border-black border-opacity-40 w-full p-1 rounded-sm dark:bg-transparent dark:border-white @error('password') border-red-500 border-opacity-100  @enderror" value="">
             </div>
-            @enderror
-        </div>
+            <div>
+                <button type="submit" class="bg-hacker-orange text-white py-2 rounded-sm w-full dark:bg-dark-gh-btn">Update settings</button>
+            </div> --}}
+        </form>
 
 
         @if ($posts->count()) 
