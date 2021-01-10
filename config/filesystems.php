0beae8af319fdc46571334cda50a359dc30c3ad1
felixgren <felixg.contact@gmail.com>
2021-01-10 14:38:32 +0100

    Updated config with AWS access info

--- config/filesystems.php
+++ config/filesystems.php
@@ -38,18 +38,16 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
         ],
 
         's3' => [
             'driver' => 's3',
-            'key' => env('AWS_ACCESS_KEY_ID'),
-            'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            'region' => env('AWS_DEFAULT_REGION'),
-            'bucket' => env('AWS_BUCKET'),
-            'url' => env('AWS_URL'),
-            'endpoint' => env('AWS_ENDPOINT'),
+            'key' => env('S3_KEY'),
+            'secret' => env('S3_SECRET_KEY'),
+            'region' => 'eu-north-1',
+            'bucket' => 'images.hackernews.com',
         ],
 
     ],
