9cf2d54fcae7342a30fc89e0246c32358762a7ba
felixgren <felixg.contact@gmail.com>
2021-01-11 23:03:49 +0100

    Initial comments migration

---
+++ database/migrations/2021_01_11_211255_create_comments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('reply_id')->constrained('comments')->nullable()->onDelete('cascade');
+            $table->integer('commentable_id');
+            $table->string('commentable_type');
+            $table->text('body');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
