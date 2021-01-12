50c96ff9825bf668b30077c508c001f7eb68dc2d
felixgren <felixg.contact@gmail.com>
2021-01-12 23:11:02 +0100

    New cloned repo

--- public/js/app.js
+++ public/js/app.js
@@ -19384,8 +19384,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /mnt/c/Users/Felix/desktop/github/hacker-news-project/hacker-news/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /mnt/c/Users/Felix/desktop/github/hacker-news-project/hacker-news/resources/css/app.css */"./resources/css/app.css");
+__webpack_require__(/*! /mnt/c/Users/Felix/desktop/github/hacker-news/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /mnt/c/Users/Felix/desktop/github/hacker-news/resources/css/app.css */"./resources/css/app.css");
 
 
 /***/ })
